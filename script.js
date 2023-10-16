03e50b135e9f1b1d04f0b3513ad5cf4f9de87086
Kiobayashi <kiobayashii@gmail.com>
2023-10-17 00:58:54 +0200

    am

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let inputBtn = document.getElementById("input-btn")
-
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+let myLeads = []
 inputBtn.addEventListener("click", function(){
-    console.log("button clicked!")
+    myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = "";
 })
+
+function renderLeads(){
+
+    ulEl.innerHTML = myLeads.map(lead => `<li><a href="${lead}" target="_blank">${lead}</a></li>`).join("");
+
+}
