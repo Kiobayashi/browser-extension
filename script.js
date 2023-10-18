56aceeedbea6bd106bce310c8ffe1b389303e2a3
Kiobayashi <kiobayashii@gmail.com>
2023-10-18 04:06:36 +0200

    siema

--- script.js
+++ script.js
@@ -7,25 +7,37 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 deletAallEl.addEventListener("click", function(){
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 
 })
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
     console.log(localStorage.getItem("myLeads"))
 })
 
-function renderLeads(){
-
-    ulEl.innerHTML = myLeads.map(lead => `<li><a href="${lead}" target="_blank">${lead}</a><button id="delete-btn">X</button></li>`).join("");
-
+function render(leads) {
+    ulEl.innerHTML = leads.map((lead, index) => `
+        <li>
+            <a href="${lead}" target="_blank">${lead}</a>
+            <button class="delete-btn" data-index="${index}">X</button>
+        </li>
+    `).join("");
 }
+
+ulEl.addEventListener("click", function(event) {
+    if (event.target.classList.contains("delete-btn")) {
+        const indexToDelete = event.target.getAttribute("data-index");
+        myLeads.splice(indexToDelete, 1);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    }
+});
