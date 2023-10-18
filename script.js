6f3523c0e869303360de03b5981e218f7870d29f
Kiobayashi <kiobayashii@gmail.com>
2023-10-18 02:54:37 +0200

    s

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 let myLeads = []
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads))
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
-    inputEl.value = "";
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads(){
